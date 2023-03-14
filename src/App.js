925cd1057b3ea610745baad2ab79a489048673b7
phongngotrong <ngotrongphong.dev@gmail.com>
2023-03-15 01:02:48 +0700

    ui banner

--- src/App.js
+++ src/App.js
@@ -4,10 +4,37 @@ import { NavLink } from "react-router-dom";
 function App() {
   return (
     <Fragment>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10">
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
         <span className="text-primary">Home</span>
         <span>Movies</span>
       </header>
+      <section className="banner h-[400px] page-container">
+        <div className="w-full h-full relative">
+          <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
+          <img
+            src="https://vtv1.mediacdn.vn/2019/4/26/poster-payoff-1-1556273680151870157160-crop-1556273779257196175768.jpg"
+            alt=""
+            className="w-full h-full object-cover rounded-lg"
+          />
+          <div className="absolute left-5 bottom-5 w-full text-white ">
+            <h2 className="font-bold text-3xl mb-5">Avenger: Endgame</h2>
+            <div className="flex items-center gap-x-3 mb-8">
+              <span className="py-2 px-4 border border-white rounded-md">
+                Adventure
+              </span>
+              <span className="py-2 px-4 border border-white rounded-md">
+                Adventure
+              </span>
+              <span className="py-2 px-4 border border-white rounded-md">
+                Adventure
+              </span>
+            </div>
+            <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">
+              Watch Now
+            </button>
+          </div>
+        </div>
+      </section>
     </Fragment>
   );
 }
