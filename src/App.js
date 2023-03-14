323a5d1815489ce68674dae1bef3e555611e401d
phongngotrong <ngotrongphong.dev@gmail.com>
2023-03-15 00:47:10 +0700

    ui menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <p className="p-3">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem rerum
-        incidunt eius a eligendi in, quis possimus at esse exercitationem minus,
-        natus dolorum, modi nam est? Tenetur, obcaecati! Harum, laborum?
-      </p>
-    </div>
+    <Fragment>
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10">
+        <span className="text-primary">Home</span>
+        <span>Movies</span>
+      </header>
+    </Fragment>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#f62682",
+      },
+    },
   },
   plugins: [],
 };
