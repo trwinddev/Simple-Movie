3c1bcb8627ed613097ba2808f19135cd91df24f1
phongngotrong <ngotrongphong.dev@gmail.com>
2023-03-14 17:17:23 +0700

    setup pj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="p-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem rerum
+        incidunt eius a eligendi in, quis possimus at esse exercitationem minus,
+        natus dolorum, modi nam est? Tenetur, obcaecati! Harum, laborum?
+      </p>
     </div>
   );
 }
