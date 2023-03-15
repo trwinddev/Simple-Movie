0f69cbdd5a91ea63645254ffb7ac713236c2fb01
phongngotrong <ngotrongphong.dev@gmail.com>
2023-03-15 17:27:59 +0700

    fetching data

--- src/components/movie/MovieList.js
+++ src/components/movie/MovieList.js
@@ -6,15 +6,12 @@ import useSWR from "swr";
 import { fetcher } from "../../config/config";
 // https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>&language=en-US&page=1
 const MovieList = ({ type = "now_playing" }) => {
-  const [movies, setMovies] = useState([]);
   const { data } = useSWR(
     `https://api.themoviedb.org/3/movie/${type}?api_key=1e736f7376d8b7f90a66c1431257159b`,
     fetcher
   );
-  useEffect(() => {
-    if (data && data.results) setMovies(data.results);
-  }, [data]);
-  console.log(movies);
+  const movies = data?.results || [];
+
   return (
     <div className="movie-list">
       <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
