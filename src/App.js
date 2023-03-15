b297fa0dc060aa90b5851427d9639adaa6e3e515
phongngotrong <ngotrongphong.dev@gmail.com>
2023-03-15 14:24:18 +0700

    ui movie card

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom";
 function App() {
   return (
     <Fragment>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-5">
         <span className="text-primary">Home</span>
         <span>Movies</span>
       </header>
-      <section className="banner h-[400px] page-container">
+      <section className="banner h-[500px] page-container mb-20">
         <div className="w-full h-full relative">
           <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
           <img
@@ -31,6 +31,72 @@ function App() {
             </div>
             <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">
               Watch Now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Now playing
+        </h2>
+        <div className="movies-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white ">
+            <img
+              src="https://vtv1.mediacdn.vn/2019/4/26/poster-payoff-1-1556273680151870157160-crop-1556273779257196175768.jpg"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-lg mb-5"
+            />
+            <h3 className="font-bold text-xl mb-3">Spiderman: Homecoming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Top rated
+        </h2>
+        <div className="movies-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white ">
+            <img
+              src="https://vtv1.mediacdn.vn/2019/4/26/poster-payoff-1-1556273680151870157160-crop-1556273779257196175768.jpg"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-lg mb-5"
+            />
+            <h3 className="font-bold text-xl mb-3">Spiderman: Homecoming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Trending
+        </h2>
+        <div className="movies-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white ">
+            <img
+              src="https://vtv1.mediacdn.vn/2019/4/26/poster-payoff-1-1556273680151870157160-crop-1556273779257196175768.jpg"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-lg mb-5"
+            />
+            <h3 className="font-bold text-xl mb-3">Spiderman: Homecoming</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+              Watch now
             </button>
           </div>
         </div>
