b50b29623e53fae0565692da34ddc43bf8fba7ed
phongngotrong <ngotrongphong.dev@gmail.com>
2023-03-25 14:23:39 +0700

    code  splitting

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import { NavLink, Route, Routes } from "react-router-dom";
 import "swiper/scss";
 import Banner from "components/banner/Banner";
-import HomePage from "components/pages/HomePage";
 import Main from "components/layout/Main";
-import MoviePage from "components/pages/MoviePage";
-import MovieDetailPage from "components/pages/MovieDetailPage";
+// import HomePage from "components/pages/HomePage";
+// import MoviePage from "components/pages/MoviePage";
+// import MovieDetailPage from "components/pages/MovieDetailPage";
+
+const HomePage = lazy(() => import("components/pages/HomePage"));
+const MoviePage = lazy(() => import("components/pages/MoviePage"));
+const MovieDetailPage = lazy(() => import("components/pages/MovieDetailPage"));
 
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route
-            path="/"
-            element={
-              <>
-                <Banner></Banner>
-                <HomePage></HomePage>
-              </>
-            }
-          ></Route>
-          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-          <Route
-            path="/movies/:movieId"
-            element={<MovieDetailPage></MovieDetailPage>}
-          ></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner></Banner>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movies/:movieId"
+              element={<MovieDetailPage></MovieDetailPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
